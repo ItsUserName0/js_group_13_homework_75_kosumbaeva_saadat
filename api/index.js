aada011a6671ddaaa0e8af6f2d5aa264b2955a3f
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-01-28 18:41:35 +0600

    Created the REST API for the Vigenere cypher app.

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const {Cipher, Decipher} = require('caesar-salad').Vigenere;
+const app = express();
+const port = 8000;
+
+app.use(cors({origin: 'http://localhost:4200'}));
+app.use(express.json());
+
+app.post('/encode', (req, res, next) => {
+  try {
+    const encodedText = Cipher(req.body.password).crypt(req.body.message);
+    return res.send({encoded: encodedText});
+  } catch (e) {
+    next(e);
+  }
+});
+app.post('/decode', (req, res, next) => {
+  try {
+    const decodedText = Decipher(req.body.password).crypt(req.body.message);
+    return res.send({decoded: decodedText});
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port`);
+});
